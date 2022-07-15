1aa295841e33d082e4fde68690fd897e2388a164
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-07-15 17:34:03 -0600

    Feat: Add the ability to place two ships on the board & show the user where their two ships are located

--- lib/game.rb
+++ lib/game.rb
@@ -13,14 +13,25 @@ class Game
           #player place ships (writing that now)
           @board = Board.new
           cruiser = Ship.new("Cruiser", 3)
+          submarine = Ship.new("Submarine", 2)
           puts @board.render
           puts "Enter the squares for the Cruiser in order (3 spaces) You can only place your ship vertically or horizontally:"
           user_choice_for_cruiser = gets.chomp.split(" ")
           unless @board.valid_placement?(cruiser, user_choice_for_cruiser)
+            #not reaching this, yet!
             puts "That isn't a valid placement."
           else
             @board.place(cruiser, user_choice_for_cruiser)
             puts @board.render(true)
+            puts "Place your 2nd ship called the Submarine (2 spaces) You cannot overlap ships, so don't try!"
+            user_choice_for_submarine = gets.chomp.split(" ")
+            unless @board.valid_placement?(submarine, user_choice_for_submarine)
+              puts "That isn't a valid placement!"
+            else
+              @board.place(submarine, user_choice_for_submarine)
+              puts @board.render(true)
+            end
+
           end
       elsif answer == "q"
         puts "Please remove yourself from the battlefield..."
@@ -33,3 +44,7 @@ class Game
 
 
 end
+
+#I think this should be added to the unless statement for validating board placement but valid_coordinate
+#can only be called on a board object & i am calling it on an array.
+#&& user_choice_for_cruiser.all? {|coordinate| valid_coordinate?(coordinate)}
