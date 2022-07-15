f4bd53ca6e4a2a38d317be6a7d68d1003fa01543
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-07-15 17:22:07 -0600

    Add cleaner conditional statements to the start method for p & q & delete the while loop and the break

--- lib/game.rb
+++ lib/game.rb
@@ -7,31 +7,27 @@ class Game
     puts "     Welcome to BATTLESHIP!!!! \n Enter p to play. Enter q to quit."
     answer = gets.chomp
 
-    while answer.downcase != "p" || answer.downcase != "q"
-      if answer == "p"
-          #game set up
+      if answer.downcase == "p"
           puts "I have laid out my ships on the grid. \nYou now need to lay out your two ships.\nThe Cruiser is three units long and the Submarine is two units long."
-          #computer place ships
-          #player place ships
+          #computer place ships before this (need to write that)
+          #player place ships (writing that now)
           @board = Board.new
           cruiser = Ship.new("Cruiser", 3)
           puts @board.render
-          puts "Enter the squares for the Cruiser (3 spaces):"
+          puts "Enter the squares for the Cruiser in order (3 spaces) You can only place your ship vertically or horizontally:"
           user_choice_for_cruiser = gets.chomp.split(" ")
-          if @board.valid_placement?(cruiser, user_choice_for_cruiser)
+          unless @board.valid_placement?(cruiser, user_choice_for_cruiser)
+            puts "That isn't a valid placement."
+          else
             @board.place(cruiser, user_choice_for_cruiser)
             puts @board.render(true)
-          else 
-            puts "That isn't a valid placement."
           end
       elsif answer == "q"
         puts "Please remove yourself from the battlefield..."
-        break
       else
         puts "Please try again using either p to play or q to quit."
         answer = gets.chomp
       end
-    end
   end
 
 
