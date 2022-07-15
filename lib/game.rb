d336a3aca042c713853cbf8677e73b9cec0b08b3
Ryan Smith <ryan.chris.smith@gmail.com>
2022-07-15 16:37:18 -0600

    feat: start method working (mostly), still needs to pass an array into the @board.place method

--- lib/game.rb
+++ lib/game.rb
@@ -6,14 +6,30 @@ class Game
   def start
     puts "     Welcome to BATTLESHIP!!!! \n Enter p to play. Enter q to quit."
     answer = gets.chomp
-    if answer.downcase == "q"
-      puts "Thank you for checking in, better luck next time!"
-    elsif
-      answer == "p"
-      #computer place ships
-      #player place ships
+
+    while answer.downcase != "p" || answer.downcase != "q"
+      if answer == "p"
+          #game set up
+          puts "I have laid out my ships on the grid. \nYou now need to lay out your two ships.\nThe Cruiser is three units long and the Submarine is two units long."
+          #computer place ships
+          #player place ships
+          @board = Board.new
+          puts @board.render
+          puts "Enter the squares for the Cruiser (3 spaces):"
+          cruiser_array = gets.chomp #still a string for an answer
+          cruiser = Ship.new("Cruiser", 3)
+          @board.place(cruiser, cruiser_array.to_a) # this is where an array needs to be passed into the 2nd argument
+          puts @board.render(true)
+      elsif answer == "q"
+        puts "Please remove yourself from the battlefield..."
+        break
+      else
+        puts "Please try again using either p to play or q to quit."
+        answer = gets.chomp
+      end
     end
   end
+
 
 
 end
